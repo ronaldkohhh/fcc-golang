9f8aa4047f0132c1eb1e7a193b1dbfe4c39a5fd5
ronaldkohhh <ronald9872@gmail.com>
2024-03-28 22:16:28 +0800

    first commit

--- course/2-variables/exercises/2-short_declarations/code.go
+++ course/2-variables/exercises/2-short_declarations/code.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 	// declare here
-
+	congrats := "Happy Birthday!"
 	fmt.Println(congrats)
 }
--- course/2-variables/exercises/4-same_line_declarations/code.go
+++ course/2-variables/exercises/4-same_line_declarations/code.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 	// declare here
-
+	averageOpenRate, displayMessage := 0.23, "is the average open rate for your message"
 	fmt.Println(averageOpenRate, displayMessage)
 }
